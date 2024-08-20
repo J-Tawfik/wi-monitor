dc2f8f6e06d1d523bb391b12307c2c26655296a3
J-Tawfik <132085160+J-Tawfik@users.noreply.github.com>
2024-08-20 20:08:37 +0100

    Update wimonitor.sh

--- wimonitor.sh
+++ wimonitor.sh
@@ -1,11 +1,30 @@
 #! /bin/bash
-echo  welcome to wi_monitor this script created by : j-tawfik
-echo //////////////////////////////////////////////////////////////////
-echo /////////////////////////////////////////////////////////////////
+
+
+banner="
+█████   ███   █████ █████ ██████   ██████    ███████    ██████   █████
+░░███   ░███  ░░███ ░░███ ░░██████ ██████   ███░░░░░███ ░░██████ ░░███
+ ░███   ░███   ░███  ░███  ░███░█████░███  ███     ░░███ ░███░███ ░███
+ ░███   ░███   ░███  ░███  ░███░░███ ░███ ░███      ░███ ░███░░███░███
+ ░░███  █████  ███   ░███  ░███ ░░░  ░███ ░███      ░███ ░███ ░░██████
+  ░░░█████░█████░    ░███  ░███      ░███ ░░███     ███  ░███  ░░█████
+    ░░███ ░░███      █████ █████     █████ ░░░███████░   █████  ░░█████
+     ░░░   ░░░      ░░░░░ ░░░░░     ░░░░░    ░░░░░░░    ░░░░░    ░░░░░
+
+
+ welcome to wi_monitor this script created by : j-tawfik  "
+
+# Print banner with formatting\033[47m
+echo -e "\033[1m\033[40m${banner}\033[0m"
+echo welocme ;,user= whoami
+echo "hello  $user"
+echo
 echo https://github.com/J-Tawfik/
 echo press enter to start
 echo
 read enter
+echo enter your password for sudo permession
+sudo
 clear
 echo starting ifconfg to select interface
 ifconfig
@@ -14,7 +33,8 @@ read interface
 echo  interface selected $interface
 echo your wifi is gonna be changed to monitor mode press enter to continue
 read enter
-airmon-ng start $interface
+sudo airmon-ng start $interface
 echo monitor mode turned on
 read enter
-airodump-ng $interface
+interface =$nterface"mon"
+sudo airodump-ng $interface
